87ca1527ed1e55b6bebab6ab236ff2bf0dcdad54
nerea [itb] arrabal lopez <nerea.arrabal.7e6@itb.cat>
2024-02-09 13:09:02 +0100

    login functionality done

--- app/src/main/java/com/example/rutescompartidesapp/view/login/LoginScreen.kt
+++ app/src/main/java/com/example/rutescompartidesapp/view/login/LoginScreen.kt
@@ -1,21 +1,249 @@
 package com.example.rutescompartidesapp.view.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.outlined.LockOpen
+import androidx.compose.material.icons.outlined.Mail
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import cafe.adriel.voyager.core.screen.Screen
-
-object LoginScreen: Screen {
-    @Composable
-    override fun Content() {
-        LoginScreen()
-
-    }
-}
 
 @Composable
 fun LoginScreen(){
+    //Email
+    val loginViewModel: LoginViewModel = hiltViewModel()
+    val userEmailError by loginViewModel.userEmailError.collectAsStateWithLifecycle()
+    val userEmailText by loginViewModel.userEmail.collectAsStateWithLifecycle()
+    //Password
+    val userPasswordError by loginViewModel.userPasswordError.collectAsStateWithLifecycle()
+    val userPasswordText by loginViewModel.userPassword.collectAsStateWithLifecycle()
+    val isPasswordVisible by loginViewModel.isPasswordVisible.collectAsStateWithLifecycle()
+    //Repeat Password
+    val userRepeatPasswordError by loginViewModel.userRepeatPasswordError.collectAsStateWithLifecycle()
+    val userRepeatPasswordText by loginViewModel.userRepeatPassword.collectAsStateWithLifecycle()
+    val isRepeatPasswordVisible by loginViewModel.isRepeatPasswordVisible.collectAsStateWithLifecycle()
 
+
+    Column(modifier= Modifier
+        .fillMaxSize()
+        .background(color = MaterialTheme.colorScheme.background),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        //Tittle
+        Row(horizontalArrangement = Arrangement.Absolute.Left) {
+            Text(
+                text = "Crear un compte",
+                color = MaterialTheme.colorScheme.primary,
+            )
+        }
+        Spacer(modifier = Modifier.padding(4.dp))
+
+        //User Email
+        Row () {
+            OutlinedTextField(value = userEmailText, onValueChange = loginViewModel::onUserEmailTextChange,
+                leadingIcon = {
+                    Icon(imageVector = Icons.Outlined.Mail, contentDescription = "user email icon", tint= Color.LightGray)
+                },
+                placeholder = {
+                    Text(text = "Correu Electrònic", color = Color.Gray)
+                },
+                shape = RoundedCornerShape(16.dp),
+                //User Email Error
+                isError = userEmailError,
+                trailingIcon = {
+                    if (userEmailError){
+                        Icon(imageVector = Icons.Filled.Cancel,
+                            contentDescription = "Error Icon",
+                            tint = MaterialTheme.colorScheme.primary)
+                    }
+                },
+                //User Email Text
+                supportingText = {
+                    if (userEmailError){
+                        Text(text = "Escriu un email vàlid",
+                            color = MaterialTheme.colorScheme.primary)
+                    }
+                },
+                //User Email Keyboard
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
+                singleLine = true
+            )
+
+        }
+        Spacer(modifier = Modifier.padding(4.dp))
+        //User Password
+        Row () {
+
+
+            OutlinedTextField(
+                value = userPasswordText, onValueChange = loginViewModel::onUserPasswordTextChange,
+
+
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.LockOpen,
+                        contentDescription = "user password icon",
+                        tint = Color.LightGray
+                    )
+                },
+                placeholder = {
+                    Text(text = "Contrassenya", color = Color.Gray)
+                },
+                shape = RoundedCornerShape(16.dp),
+
+                //User Password Visibility
+                visualTransformation =
+                if (isPasswordVisible) {
+                    VisualTransformation.None
+                } else {
+                    PasswordVisualTransformation()
+                },
+
+                //User Password Error
+                isError = userPasswordError,
+                trailingIcon = {
+                    val image =
+                        if (isPasswordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                    IconButton(onClick = {loginViewModel.togglePasswordVisibility()}) {
+                        Icon(imageVector = image, contentDescription = "toggle password icon" )
+                    }
+                    if (userPasswordError) {
+                        Icon(
+                            imageVector = Icons.Filled.Cancel,
+                            contentDescription = "Error Icon",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
+
+                //User Password Text
+                supportingText = {
+
+                    if (userPasswordError) {
+                        Text(
+                            text = "Escriu una contrassenya vàlida",
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
+
+                //User Password Keyboard
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Next
+                ),
+                singleLine = true,
+
+                )
+
+        }
+        Spacer(modifier = Modifier.padding(4.dp))
+
+        //User Repeat Password
+        Row () {
+            OutlinedTextField(value = userRepeatPasswordText, onValueChange = loginViewModel::onUserRepeatPasswordTextChange,
+                leadingIcon = {
+                    Icon(imageVector = Icons.Outlined.LockOpen, contentDescription = "user password icon", tint= Color.LightGray)
+                },
+                placeholder = {
+                    Text(text = "Pepeteix Contrassenya", color = Color.Gray)
+                },
+                shape = RoundedCornerShape(16.dp),
+
+                //User Repeat Password Visibility
+                visualTransformation =
+                if (isRepeatPasswordVisible) {
+                    VisualTransformation.None
+                } else {
+                    PasswordVisualTransformation()
+                },
+
+                //User Repeat Password Error
+                isError = userRepeatPasswordError,
+                trailingIcon = {
+                    Row (horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(0.dp,0.dp,8.dp,0.dp)) {
+                        val image =
+                            if (isRepeatPasswordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                        IconButton(onClick = {loginViewModel.toggleRepeatPasswordVisibility()}) {
+                            Icon(imageVector = image, contentDescription = "toggle password icon" )
+                        }
+                        if (userRepeatPasswordError){
+                            Icon(imageVector = Icons.Filled.Cancel,
+                                contentDescription = "Error Icon",
+                                tint = MaterialTheme.colorScheme.primary)
+                        }
+                    }
+
+                },
+
+
+                //User Repeat Password Text
+                supportingText = {
+                    if (userRepeatPasswordError){
+                        Text(text = "Les contrassenyes no coincideixen",
+                            color = MaterialTheme.colorScheme.primary)
+                    }
+                },
+                //User Repeat Password Keyboard
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Next
+                ),
+                singleLine = true
+            )
+
+        }
+        Spacer(modifier = Modifier.padding(4.dp))
+
+        //Sing Up Button
+        Row (){
+            ElevatedButton(
+                onClick = loginViewModel::onSignUpButtonClick,
+                modifier = Modifier.fillMaxWidth(0.7f),
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.secondary
+                ),
+                //enabled = false,
+                shape = RoundedCornerShape(16.dp)
+            ) {
+                Text(text = "Envia")
+            }
+        }
+    }
 }
 
 @Preview
--- app/src/main/java/com/example/rutescompartidesapp/view/login/LoginViewModel.kt
+++ app/src/main/java/com/example/rutescompartidesapp/view/login/LoginViewModel.kt
@@ -1,10 +1,114 @@
 package com.example.rutescompartidesapp.view.login
 
+import android.util.Patterns
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 class LoginViewModel: ViewModel() {
+    //User email text
+    private val _userEmail = MutableStateFlow("")
+    val userEmail = _userEmail.asStateFlow()
+
+    fun onUserEmailTextChange(text: String) {
+        _userEmail.value = text
+
+        //Check if the email is correct and update error status
+        val userEmail = _userEmail.value
+        val isError = userEmail != text
+
+        onUserEmailError(isError)
+    }
+
+    //User email error
+    private val _userEmailError = MutableStateFlow(false)
+    val userEmailError = _userEmailError.asStateFlow()
+
+    fun onUserEmailError(isError: Boolean) {
+        _userEmailError.value = isError
+    }
+
+    //User password text
+    private val _userPassword = MutableStateFlow("")
+    val userPassword = _userPassword.asStateFlow()
+
+    fun onUserPasswordTextChange(text: String) {
+        _userPassword.value = text
+    }
+
+    //Password visibility
+    private val _isPasswordVisible = MutableStateFlow(false)
+    val isPasswordVisible = _isPasswordVisible.asStateFlow()
+
+    fun togglePasswordVisibility() {
+        _isPasswordVisible.value = !_isPasswordVisible.value
+    }
+
+    //Repeat Password visibility
+    private val _isRepeatPasswordVisible = MutableStateFlow(false)
+    val isRepeatPasswordVisible = _isPasswordVisible.asStateFlow()
+
+    fun toggleRepeatPasswordVisibility() {
+        _isPasswordVisible.value = !_isPasswordVisible.value
+    }
+
+    //User password error
+    private val _userPasswordError = MutableStateFlow(false)
+    val userPasswordError = _userPasswordError.asStateFlow()
+
+    fun onUserPasswordError(isError: Boolean) {
+        _userPasswordError.value = isError
+    }
+
+    //User password hide and show
+
+    //User repeat password text
+    private val _userRepeatPassword = MutableStateFlow("")
+    val userRepeatPassword = _userRepeatPassword.asStateFlow()
 
 
+    fun onUserRepeatPasswordTextChange(text: String) {
+        _userRepeatPassword.value = text
+
+        // Check if passwords match and update error status
+        val userPassword = _userPassword.value
+        val isError = userPassword != text
+
+        onUserRepeatPasswordError(isError)
+    }
+
+    //User repeat password error
+    private val _userRepeatPasswordError = MutableStateFlow(false)
+    val userRepeatPasswordError = _userRepeatPasswordError.asStateFlow()
+
+    fun onUserRepeatPasswordError(isError: Boolean) {
+        _userRepeatPasswordError.value = isError
+    }
+
+    //Sing Up Button
+    fun onSignUpButtonClick() {
+
+        // Variables to obtain the values of the input
+        val userEmail = _userEmail.value
+        val userPassword = _userPassword.value
+        val userRepeatPassword = _userRepeatPassword.value
+
+        // Verify if any field is empty
+        //val isAnyFieldEmpty = userName.isEmpty() || userEmail.isEmpty() || userPhone.isEmpty() || userPassword.isEmpty() || userRepeatPassword.isEmpty()
+
+
+        //Check the Email format
+        if (!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()) {
+            onUserEmailError(isError = true)
+
+        }
+
+        onUserEmailError(userEmail.isEmpty())
+        onUserPasswordError(userPassword.isEmpty())
+        onUserRepeatPasswordError(userRepeatPassword.isEmpty())
+
+
+    }
 }
