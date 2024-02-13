a60cf6bc9206f24de0480471f730df1befad2b30
Ivan Martinez Cañero <ivanmartinezca10@gmail.com>
2024-02-13 14:30:10 +0100

    Added user and punts intermigs in card item of list

--- app/src/main/java/com/example/rutescompartidesapp/view/routes_order_list/components/ListCards.kt
+++ app/src/main/java/com/example/rutescompartidesapp/view/routes_order_list/components/ListCards.kt
@@ -1,6 +1,5 @@
 package com.example.rutescompartidesapp.view.routes_order_list.components
 
-import android.widget.Space
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
