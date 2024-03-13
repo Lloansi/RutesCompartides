649f5f0d652d55b46651207857d5e7e9f71ad29e
AlejandroAndroidDev <alejandro.arcas.7e5@itb.cat>
2024-03-13 11:17:15 +0100

    [ComFunciona] {Fix} Fixing email client chooser (link)

--- app/src/main/java/com/example/rutescompartidesapp/view/com_funciona/ComFuncionaScreen.kt
+++ app/src/main/java/com/example/rutescompartidesapp/view/com_funciona/ComFuncionaScreen.kt
@@ -1,20 +1,29 @@
 package com.example.rutescompartidesapp.view.com_funciona
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.support.v4.media.MediaBrowserCompat
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -22,14 +31,24 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavHostController
+import com.example.rutescompartidesapp.R
 import com.example.rutescompartidesapp.ui.theme.MateBlackRC
 import com.example.rutescompartidesapp.ui.theme.fredokaOne
 import com.example.rutescompartidesapp.ui.theme.openSans
@@ -55,7 +74,11 @@ fun ComFuncionaScreen(navController: NavHostController) {
         ) {
             HeaderSphere(height = 200.dp)
 
-            BackButtonArrow(navController = navController, alignment = Alignment.TopStart, "ProfileScreen")
+            BackButtonArrow(
+                navController = navController,
+                alignment = Alignment.TopStart,
+                "ProfileScreen"
+            )
 
             Text(
                 modifier = Modifier
@@ -72,8 +95,13 @@ fun ComFuncionaScreen(navController: NavHostController) {
             modifier = Modifier
                 .verticalScroll(rememberScrollState())
         ) {
-            YouTubePlayer(youtubeVideoId = "85IINPA_igE", lifecycleOwner = LocalLifecycleOwner.current)
+            YouTubePlayer(
+                youtubeVideoId = "85IINPA_igE",
+                lifecycleOwner = LocalLifecycleOwner.current
+            )
             Description()
+            RRSSButtons()
+            Spacer(modifier = Modifier.size(height = 20.dp, width = 0.dp))
         }
         Spacer(modifier = Modifier.size(height = 20.dp, width = 0.dp))
 
@@ -124,8 +152,112 @@ fun Description() {
         fontFamily = openSans,
         fontWeight = FontWeight.SemiBold,
         color = if (isSystemInDarkTheme()) Color.White else MateBlackRC,
-        fontSize = 15.5.sp,
+        fontSize = 14.5.sp,
         modifier = Modifier
             .padding(20.dp)
     )
+}
+
+@Composable
+fun RRSSButtons() {
+
+    val ctx = LocalContext.current
+    val mUriHandler = LocalUriHandler.current
+
+    val annotatedEmailText = buildAnnotatedString {
+        pushStringAnnotation(tag = "email", annotation = "mailto:rutescompartides@riseup.net")
+        withStyle(style = SpanStyle(color = if (isSystemInDarkTheme()) Color.White else MateBlackRC)) {
+            append("rutescompartides@riseup.net")
+        }
+        pop()
+    }
+
+    val annotatedTelegramString = buildAnnotatedString {
+        pushStringAnnotation(
+            tag = "telegram",
+            annotation = "https://t.me/joinchat/-DCYs7jkCFRiOWY5"
+        )
+        withStyle(style = SpanStyle(color = if (isSystemInDarkTheme()) Color.White else MateBlackRC)) {
+            append("Grup de Telegram")
+        }
+        pop()
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(start = 20.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.email_icon_comfunciona),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(30.dp)
+                    .height(30.dp)
+                    .padding(end = 8.dp)
+            )
+            ClickableText(
+                text = annotatedEmailText,
+                style = TextStyle(
+                    color = if (isSystemInDarkTheme()) Color.White else MateBlackRC,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = openSans,
+                    fontSize = 13.sp
+                ),
+                onClick = { offset ->
+                    annotatedEmailText.getStringAnnotations(
+                        tag = "email",
+                        start = offset,
+                        end = offset
+                    ).firstOrNull()?.let {
+                        ctx.sendMail()
+                    }
+                })
+        }
+        Spacer(modifier = Modifier.size(height = 5.dp, width = 0.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.telegram_icon_comfunciona),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(30.dp)
+                    .height(30.dp)
+                    .padding(end = 8.dp)
+            )
+            ClickableText(
+                text = annotatedTelegramString,
+                style = TextStyle(
+                    color = if (isSystemInDarkTheme()) Color.White else MateBlackRC,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = openSans,
+                    fontSize = 13.sp
+                ),
+                onClick = { offset ->
+                    annotatedTelegramString.getStringAnnotations(
+                        tag = "telegram",
+                        start = offset,
+                        end = offset
+                    ).firstOrNull()?.let {
+                        mUriHandler.openUri(it.item)
+                    }
+                })
+        }
+    }
+}
+
+fun Context.sendMail() {
+    try {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "vnd.android.cursor.item/email"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("rutescompartides@riseup.net"))
+        startActivity(intent)
+    } catch (e: ActivityNotFoundException) {
+        println("PRIMER ERROR$e")
+    } catch (t: Throwable) {
+        println("SEGUNDO ERROR$t")
+    }
 }
