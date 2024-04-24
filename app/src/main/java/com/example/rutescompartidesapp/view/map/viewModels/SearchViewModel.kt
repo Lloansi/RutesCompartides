922104cec496f5bcb087071e02662388197d45b0
David <dglloansi@gmail.com>
2024-04-24 17:05:23 +0200

    [MapScreen] {Add} Filter per user input in search bar

--- app/src/main/java/com/example/rutescompartidesapp/view/map/viewModels/SearchViewModel.kt
+++ app/src/main/java/com/example/rutescompartidesapp/view/map/viewModels/SearchViewModel.kt
@@ -2,6 +2,8 @@ package com.example.rutescompartidesapp.view.map.viewModels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.rutescompartidesapp.data.domain.Order
+import com.example.rutescompartidesapp.data.domain.OrderForList
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
@@ -11,9 +13,15 @@ import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import com.example.rutescompartidesapp.data.domain.Route
+import com.example.rutescompartidesapp.data.domain.RouteForList
 import kotlinx.coroutines.FlowPreview
 
 class SearchViewModel: ViewModel() {
+
+    data class FilteredDataMapSearchBar(
+        val routesFiltered: List<RouteForList>,
+        val ordersFiltered: List<OrderForList>
+    )
 
     private val _searchText = MutableStateFlow("")
     val searchText = _searchText.asStateFlow()
@@ -23,6 +31,12 @@ class SearchViewModel: ViewModel() {
 
     private val _routes = MutableStateFlow(listOf<Route>())
     val routes = _routes.asStateFlow()
+
+    private val _orders = MutableStateFlow(listOf<Order>())
+    val orders = _orders.asStateFlow()
+
+    private val _ordersAndRoutes = MutableStateFlow(listOf<Pair<List<Route>, List<Order>>>())
+    val ordersAndRoutes = _ordersAndRoutes.asStateFlow()
 
     fun onSearchTextChange(text:String){
         _searchText.value = text
@@ -36,7 +50,7 @@ class SearchViewModel: ViewModel() {
         .onEach{ _isSearching.update{ true } }
         // combine lets the developer to use properties from more than one element, as we can see, we can deal with text and routes at the same time
         .combine(_routes) { text, routes ->
-            // if user dont writes anything, we return all the list of routes
+            // if user dont writes anything, we return all the list of routesAndOrders
             if (text.isBlank()) {
                 routes
             // else , we apply a filter to show routes that contains the letters
@@ -54,6 +68,40 @@ class SearchViewModel: ViewModel() {
         _isSearching.value = !_isSearching.value
         if (!_isSearching.value) {
             onSearchTextChange("")
+        }
+    }
+
+    fun updateSearchedOrdersAndRoutes(text: String) {
+        val filteredRoutes = filterRoutesBySearchText(text)
+        val filteredOrders = filterOrdersBySearchText(text)
+
+        _routes.value = filteredRoutes
+        _orders.value = filteredOrders
+
+        _ordersAndRoutes.value = listOf(Pair(_routes.value, _orders.value))
+    }
+
+    private fun filterRoutesBySearchText(searchText: String): List<Route> {
+        val allRoutes = listOf<Route>() // TODO Remplazarlo por una llamada a una api con todas las rutas
+
+        return if (searchText.isNotBlank()){
+            allRoutes.filter { route ->
+                route.startPoint.contains(searchText, ignoreCase = true) || route.endPoint.contains(searchText, ignoreCase = true)
+            }
+        } else{
+            allRoutes
+        }
+    }
+
+    private fun filterOrdersBySearchText(searchText: String): List<Order> {
+        val allOrders = listOf<Order>() // TODO Remplazarlo por una llamada a una api con todas las rutas
+
+        return if (searchText.isNotBlank()){
+            allOrders.filter { order ->
+                order.packageStartPoint.contains(searchText, ignoreCase = true) || order.packageEndPoint.contains(searchText, ignoreCase = true)
+            }
+        } else{
+            allOrders
         }
     }
 }
